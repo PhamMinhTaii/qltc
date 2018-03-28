0040f33f414f9b06ab405b0b4162dfb52768bdad
phamminhtaii <96phamminhtai@gmail.com>
2018-03-28 18:24:03 +0700

    Khoi tao Project

--- src/Login/Login.java
+++ src/Login/Login.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package demobtl;
+package Login;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
  *
  * @author Minh Taii
  */
-public class DemoBTL extends Application {
+public class Login extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
