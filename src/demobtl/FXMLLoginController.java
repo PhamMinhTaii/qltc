397b163b38f43f3c950457c479b008bb948a38f8
phamminhtaii <96phamminhtai@gmail.com>
2018-03-28 16:27:29 +0700

    Khoi tao project

---
+++ src/demobtl/FXMLLoginController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demobtl;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Minh Taii
+ */
+public class FXMLLoginController implements Initializable {
+    
+    @FXML
+    private TextField txtUser;
+    @FXML
+    private TextField txtPassWord;
+        
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void loginAction(ActionEvent event) throws IOException {
+         Parent root = FXMLLoader.load(getClass().getResource("/main/FXMLMain.fxml"));        
+        Scene scene = new Scene(root);
+        Stage stage=new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void exitAction(ActionEvent event) {
+    }
+    
+}
---
+++ src/main/FXMLMainController.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Minh Taii
+ */
+public class FXMLMainController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        
+    }    
+
+    @FXML
+    private void datTiecCuoiAction(ActionEvent event) throws IOException {
+         Parent root = FXMLLoader.load(getClass().getResource("/Books/FXMLBook.fxml"));        
+        Scene scene = new Scene(root);
+        Stage stage=new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+}
